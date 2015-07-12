ad4665a02a718f18455322ef8f9452aeab7e740d
Linda Schey <lindaschey@t-online.de>
2015-07-12 17:48:36 +0200

    swig output renamed

--- SwigIt/CsClient/Program.cs
+++ SwigIt/CsClient/Program.cs
@@ -14,7 +14,7 @@ namespace CsClient
             var cppapixx = new CppApiWrapper.CppApiXX();
             var ret = cppapixx.TestArrayOut();
             ret = cppapixx.TestArrayInOut(new float3(7, 8, 9));
-            Console.WriteLine("(x=" + ret.x + "; y=" + ret.x + "; z=" + ret.z + ")");
+            Console.WriteLine("(x=" + ret.x + "; y=" + ret.y + "; z=" + ret.z + ")");
             cppapixx.TestArrayIn(new Fusee.Math.float3(5, 5, 5));
             ret = cppapixx.FooInArrayOut(new CppApiWrapper.Foo());
             Console.WriteLine("(x=" + ret.x + "; y=" + ret.x + "; z=" + ret.z + ")");
