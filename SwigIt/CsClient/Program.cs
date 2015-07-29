934fbfd1f4ca6079c3e5ca0546fc3ffe3a35d7e7
Linda Schey <lindaschey@t-online.de>
2015-07-29 16:05:13 +0200

    set the solution from debug to release

--- SwigIt/CsClient/Program.cs
+++ SwigIt/CsClient/Program.cs
@@ -12,36 +12,10 @@ namespace CsClient
     {
         static void Main(string[] args)
         {
+            //Test code here
 
-            var cppapixx = new CppApiWrapper.CppApiXX();
-            var ret = cppapixx.TestArrayOut();
-            ret = cppapixx.TestArrayInOut(new float3(7, 8, 9));
-            Console.WriteLine("(x=" + ret.x + "; y=" + ret.y + "; z=" + ret.z + ")");
-            cppapixx.TestArrayIn(new Fusee.Math.float3(5, 5, 5));
-            ret = cppapixx.FooInArrayOut(new CppApiWrapper.Foo());
-            Console.WriteLine("(x=" + ret.x + "; y=" + ret.x + "; z=" + ret.z + ")");
-            Console.WriteLine("(x=" + ret.x + "; y=" + ret.x + "; z=" + ret.z + ")");
-
-            var bla = cppapixx.default_value();
-            var s = "narf";
-            cppapixx.default_value(s);
-
-            
-            var theVector = cppapixx.createV3(4, 5, 6);
-            Console.WriteLine("theVector[0]=" + theVector[0] + " theVector[1]= " + theVector[1] +" theVector[2]= " + theVector[2]);
-
-
-            var map = cppapixx.properties();
-            int val;
-            var retmap = map.TryGetValue("first", out val);
-            Console.WriteLine("map= " + map+ " val= " + val+ " retmap= " + retmap);
-
-            cppapixx.TestArrayInObsolete(ret);
-            var t = cppapixx.TestArrayOutObsolete();
-            t = cppapixx.FooInArrayOutObsolete(new Foo());
+            Console.WriteLine("Press any key to continue");           
             Console.ReadKey();
-
-
         }
     }
 }
